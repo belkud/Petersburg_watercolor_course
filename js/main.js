b7fb1a3326706880253de563eb1930f318999ebe
belkud <vlad-belkud@yandex.ru>
2026-02-26 11:37:08 +0300

    make section with materials for course

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+$(document).ready(function () {
+
+    initApp();
+
+    function initApp() {
+        // Список материалов
+        initMaterialsList();
+
+    }
+
+    function initMaterialsList() {
+        const $container = $('#materialsContainer');
+        const $listContainer = $('.materials-list-container'); // Изменили селектор
+        const $toggleBtn = $('.toggle-list-btn');
+
+        if ($toggleBtn.length) {
+            $toggleBtn.on('click', function () {
+                $listContainer.slideToggle(300);
+                $container.toggleClass('active');
+            });
+
+            $(document).on('click', function (e) {
+                if (!$container.is(e.target) &&
+                    $container.has(e.target).length === 0 &&
+                    $listContainer.is(':visible')) {
+                    $listContainer.slideUp(300);
+                    $container.removeClass('active');
+                }
+            });
+        }
+    }
+
+});
